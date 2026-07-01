47a438afc87bf4e1e687fa793af636a7dd248564
Sieger13 <cpenlington@yahoo.co.nz>
2026-07-01 17:21:38 +1200

    Add files via upload

---
+++ HelloWorldExample.java
@@ -0,0 +1,18 @@
+package samplePackage;
+
+
+import java.util.Scanner;
+
+public class HelloWorldExample {
+    public static void main(String[] args) {
+        String name;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter Your Name: ");
+        name = sc.nextLine();
+        
+        System.out.println("Hello! " + name);
+        
+        sc.close();
+    }
+}
